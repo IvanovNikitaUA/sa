98846cc2c26e353777d17f51b9e83eb9fb785795
Nikita <nickman.ads@gmail.com>
2017-05-15 22:39:23 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,20 @@
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['email'])&&$_POST['email']!="")&&(isset($_POST['message'])&&$_POST['message']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'ivanov_nikita2851@mail.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Сообщение с songs-animated'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>E-mail: '.$_POST['email'].'</p>    
+                        <p>Сообщение: '.$_POST['message'].'</p> 
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: SA <connect@songs-animated.co.ua>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
